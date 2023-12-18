4297d270c2083d4939e70fea718075f7775f62b1
Andres Tamayo Martinez <19030296@itcelaya.edu.mx>
2023-12-18 17:32:46 -0600

    Country.service creation with methods

--- src/app.js
+++ src/app.js
@@ -1,3 +1,14 @@
-import express from "expres";
+import express from "express";
+import db from "./config/db.js";
 
 const app = express();
+
+app.set("port", process.env.PORT || 3000);
+
+app.use("/", (req, res) => {
+    res.send("Welcome to API");
+});
+
+app.listen(app.get("port"), () => {
+    console.log("server is running on port ", app.get("port"));
+});
---
+++ src/services/provider.service.js
@@ -0,0 +1,51 @@
+import db from "../config/db.js";
+
+//Get Providers Method
+export const getProviders = () => {
+    return new Promise((resolve, reject) => {
+        const query = "SELECT * FROM proveedor";
+        db.execute(query)
+            .then((result) => resolve(result))
+            .catch((err) => reject(err));
+    });
+};
+
+//Get Provider Method
+export const getProvider = (id) => {
+    return new Promise((resolve, reject) => {
+        const query = "SELECT * FROM proveedor WHERE id = ?";
+        db.execute(query, [id])
+            .then((result) => resolve(result))
+            .catch((err) => reject(err));
+    });
+};
+
+//Create Provider Method
+export const createProvider = (provider) => {
+    return new Promise((resolve, reject) => {
+        const query = "INSERT INTO proveedor (razon_social, nombre, telefono)";
+        const {business, name, phone} = provider;
+        db.execute(query, [business, name, phone])
+            .then((result) => resolve(result))
+            .catch((err) => reject(err));
+    });
+};
+
+export const updateProvider = (id, provider) => {
+    return new Promise((resolve, reject) => {
+        const query = "UPDATE proveedor SET razon_social = ?, nombre = ?, phone = ? WHERE id = ?";
+        const {business, name, phone} = provider;
+        db.execute(query, [business, name, phone, id])
+            .then((result) => resolve(result))
+            .catch((err) => reject(err));
+    });
+};
+
+export const deleteProvider = (id) => {
+    return new Promise((resolve, reject) => {
+        const query = "DELETE FROM provider WHERE id = ?";
+        db.execute(query, [id])
+            .then((result) => resolve(result))
+            .catch((err) => reject(err));
+    });
+};
