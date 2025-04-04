202d214c7870b4166089d128aa3f734b234a8842
AndresTamayoMartinez <tamayodevelop@gmail.com>
2025-04-04 14:41:02 -0600

    Facturama controller modification

--- src/controllers/facturama.controller.js
+++ src/controllers/facturama.controller.js
@@ -6,7 +6,7 @@ export const getPaymentsForms = async (req, res) => {
         .then((result) => {
             res.status(200).json({
                 message: "Payments Forms retrieved successfully",
-                data: result[0],
+                data: result,
             })
         }).catch((err) => {
             res.status(500).send(err)
